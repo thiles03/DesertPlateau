2dcfe71c9753c79ed9f6e75cb2e69954350b7913
thiles03 <thiles03@gmail.com>
2021-01-07 14:33:09 +0000

    Initial commit

---
+++ Source/DesertPlateau/DesertPlateauGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DesertPlateauGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESERTPLATEAU_API ADesertPlateauGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
